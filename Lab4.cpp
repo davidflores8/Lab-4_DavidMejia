f599c787a8c477a8460f3fbe13ccc44f1210b1f5
davidflores8 <davidflores126@unitec.edu>
2019-05-10 13:44:03 -0600

    Primer commit

---
+++ Lab4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::stringstream;
+
+int main()
+{
+	string principal="";
+
+
+	return 0;	
+}
